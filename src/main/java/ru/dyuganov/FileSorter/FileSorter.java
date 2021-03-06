1544880de30fcf2f75be4f7662c403940144019c
dyuganov-n <dyuganov.n@gmail.com>
2021-03-06 15:42:11 +0700

    Creation date parse added

--- src/main/java/ru/dyuganov/FileSorter/FileSorter.java
+++ src/main/java/ru/dyuganov/FileSorter/FileSorter.java
@@ -1,5 +1,25 @@
 package main.java.ru.dyuganov.FileSorter;
 
-public class FileSorter {
+import java.io.File;
+import java.io.IOException;
+import java.nio.file.Files;
+import java.nio.file.Path;
+import java.nio.file.Paths;
+import java.nio.file.attribute.BasicFileAttributes;
+import java.util.Objects;
 
+public class FileSorter {
+    public final void sort(final File folder) throws IOException {
+        if (folder == null) {
+            throw new IllegalArgumentException("FileSorter got folder == null.");
+        }
+        for(final File fileEntry : Objects.requireNonNull(folder.listFiles())){
+            if(fileEntry.isDirectory()){
+                continue;
+            }
+            Path path = Paths.get(fileEntry.getPath());
+            BasicFileAttributes attributes = Files.readAttributes(path, BasicFileAttributes.class);
+            System.out.println("Creation date: " + attributes.creationTime());
+        }
+    }
 }
--- src/main/java/ru/dyuganov/Main.java
+++ src/main/java/ru/dyuganov/Main.java
@@ -1,7 +1,26 @@
 package main.java.ru.dyuganov;
 
-public class Main {
-    public static void main(String[] args) {
+import main.java.ru.dyuganov.FileSorter.FileSorter;
 
+import java.io.BufferedReader;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStreamReader;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+        try{
+            FileSorter fileSorter = new FileSorter();
+            String path;
+            System.out.println("Enter folder to sort path:");
+            try (BufferedReader reader = new BufferedReader(new InputStreamReader(System.in))) {
+                path = reader.readLine();
+                final File folder = new File(path);
+                fileSorter.sort(folder);
+            }
+        }
+        catch (RuntimeException e){
+            e.printStackTrace(System.err);
+        }
     }
 }
